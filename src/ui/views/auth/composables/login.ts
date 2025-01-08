30477b6edd6c1277e9946d47ddfc76d906c98b23
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-01-08 14:56:11 -0600

    feat(auth): :sparkles: Setear token de example en login

--- src/ui/views/auth/composables/login.ts
+++ src/ui/views/auth/composables/login.ts
@@ -22,6 +22,7 @@ export function useLogin() {
 
       const { id, name, username } = user.toValue()
 
+      authStore.setToken('token_example')
       authStore.setUser({ id, name, username }, true)
 
       alertSuccess('Login success !')
