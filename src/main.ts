783a5ffad954b43feb95801c2d1d440a60fd6a7d
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-01-08 10:48:20 -0600

    feat(main): :sparkles: Integrar vue-toastification para notificaciones

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import '@/ui/assets/main.css'
 import '@limonische/vue3-transition-expand/dist/style.css'
-
+import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import globalComponents from './plugins/component-global'
 import App from '@/ui/App.vue'
 import router from './router'
+import Toast, { type PluginOptions } from 'vue-toastification'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(globalComponents)
+app.use(Toast, {} as PluginOptions)
 
 app.mount('#app')
