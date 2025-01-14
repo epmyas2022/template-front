33a6a75fefa975a59958d65c0101a5e530ffc206
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-01-14 15:47:01 -0600

    feat(NavBarComponent): ✨ Agregar modal de perfil con campos de entrada

--- src/ui/components/NavBarComponent.vue
+++ src/ui/components/NavBarComponent.vue
@@ -3,6 +3,69 @@
     <button @click="$emit('openMenu')" :class="{ 'sm:hidden': true }">
       <IconMenu2 />
     </button>
+
+    <ModalComponent ref="profileModal" width="23rem">
+      <div class="mb-4 grid place-items-center">
+        <AvatarComponent :size="4"></AvatarComponent>
+
+        <h1 class="text-white text-lg font-bold my-1">John Doe</h1>
+        <p class="text-gray-300">@johnuser</p>
+      </div>
+      <div class="flex flex-col gap-3">
+        <div class="flex gap-3 sm:items-center flex-col sm:flex-row">
+          <TextInputComponent
+            type="text"
+            name="name"
+            autocomplete="off"
+            placeholder="Name"
+            class="text-white"
+          >
+          </TextInputComponent>
+
+          <TextInputComponent
+            type="text"
+            name="lastname"
+            autocomplete="off"
+            placeholder="Lastname"
+            class="text-white"
+          >
+          </TextInputComponent>
+        </div>
+
+        <div class="flex gap-3 sm:items-center flex-col sm:flex-row">
+          <TextInputComponent
+            type="text"
+            name="username"
+            autocomplete="off"
+            placeholder="Username"
+            class="text-white"
+          >
+          </TextInputComponent>
+
+          <TextInputComponent
+            type="text"
+            name="phone"
+            autocomplete="off"
+            placeholder="Phone"
+            class="text-white"
+          >
+          </TextInputComponent>
+        </div>
+
+        <TextInputComponent
+          type="email"
+          name="email"
+          autocomplete="off"
+          placeholder="Email"
+          class="text-white"
+        >
+        </TextInputComponent>
+
+        <div class="mt-4">
+          <button class="bg-custom-primary text-white w-full py-2 rounded-md">Save</button>
+        </div>
+      </div>
+    </ModalComponent>
     <div class="flex justify-end px-4">
       <div class="flex gap-10 items-center">
         <div class="flex gap-4">
@@ -24,20 +87,12 @@
             <IconAdjustments />
           </button>
         </div>
-        <div
-          class="w-10 h-10 rounded-full ring-custom-primary hover:ring-white ring-2 border-2 cursor-pointer relative group z-10"
-        >
-          <img
-            src="https://i.pravatar.cc/300"
-            alt="avatar"
-            class="w-full object-cover rounded-full aspect-[10/10]"
-          />
-
+        <AvatarComponent>
           <div class="absolute h-auto hidden group-hover:block right-0">
             <div class="bg-appbar-background rounded-md mt-2 px-10 py-4">
               <button
                 class="text-white text-sm w-full py-1 flex gap-2 items-center"
-                @click="router.push('/profile')"
+                @click="profileModal?.open()"
               >
                 <IconUser />
                 Profile
@@ -49,7 +104,7 @@
               </button>
             </div>
           </div>
-        </div>
+        </AvatarComponent>
       </div>
     </div>
   </nav>
@@ -64,8 +119,10 @@ import {
   IconUser,
   IconLogout2,
 } from '@tabler/icons-vue'
-
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
+import { useTemplateRef } from 'vue'
+import { type ComponentExposed } from 'vue-component-type-helpers'
+import type ModalComponent from './ModalComponent.vue'
+import TextInputComponent from './TextInputComponent.vue'
+import AvatarComponent from './AvatarComponent.vue'
+const profileModal = useTemplateRef<ComponentExposed<typeof ModalComponent>>('profileModal')
 </script>
