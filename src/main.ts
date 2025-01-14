f20f57eebfa0b0a89adc016d95430dc9459ac9c4
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-01-14 15:45:13 -0600

    feat(modal): 🎉 Agregar soporte para vue-final-modal en la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import '@/ui/assets/main.css'
+import 'vue-final-modal/style.css'
 import '@limonische/vue3-transition-expand/dist/style.css'
 import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
@@ -7,10 +8,12 @@ import globalComponents from './plugins/component-global'
 import App from '@/ui/App.vue'
 import router from './router'
 import Toast, { type PluginOptions } from 'vue-toastification'
+import { createVfm } from 'vue-final-modal'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(createVfm())
 app.use(router)
 app.use(globalComponents)
 app.use(Toast, {} as PluginOptions)
