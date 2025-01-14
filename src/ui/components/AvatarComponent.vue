fb55d5454c9494c4208854a7d4ecbbb0508c1347
Isaac <isaacosvaldo.castillo@salud.gob.sv>
2025-01-14 15:45:35 -0600

    feat(AvatarComponent):  ✨  Agregar componente Avatar con soporte para tamaño personalizado

---
+++ src/ui/components/AvatarComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :style="{ width: aspect.size, height: aspect.size }"
+    class="`w-10 h-10 rounded-full ring-custom-primary hover:ring-white ring-2 border-2 cursor-pointer relative group z-10"
+  >
+    <img
+      src="https://i.pravatar.cc/300"
+      alt="avatar"
+      class="w-full object-cover rounded-full aspect-[10/10]"
+    />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  size?: number
+}>()
+
+const aspect = computed(() => {
+  const size = props.size || '2.5'
+  return { size: `${size}rem` }
+})
+</script>
